8b75dedab6088f43792a17260082fa85b50bc098
Dadovsky <86344032+Dadovsky@users.noreply.github.com>
2023-02-06 16:16:53 +0100

    Update Decollage.py

--- Decollage.py
+++ Decollage.py
@@ -1,9 +1,14 @@
 from djitellopy import Tello
 
+#connexion drone tello
 tello = Tello()
 tello.connect()
+
+#Test de la batterie
 batterie = tello.get_battery()
 print ("La batterie est chargée à", batterie, "%")
+
+#Instructions de vol
 tello.takeoff()
 tello.move_forward(100)
 tello.move_up(100)
